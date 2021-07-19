000858894ee5ec78dca2f47df49ba68d5f494486
lzm <27617839@qq.com>
2021-07-19 11:06:58 +0800

    add button

--- app/src/main/java/lzm/com/testforall/Login.java
+++ app/src/main/java/lzm/com/testforall/Login.java
@@ -2,6 +2,7 @@ package lzm.com.testforall;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,14 +10,21 @@ import android.widget.Toast;
 public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Button button;
+        Button button,button2;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(Login.this,"hellow",Toast.LENGTH_LONG).show();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Login.this,"very goods",Toast.LENGTH_LONG).show();
             }
         });
 
