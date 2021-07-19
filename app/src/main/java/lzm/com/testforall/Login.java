f2a9d6a0ffa8352f6c9bab9ce441a0a1d126803d
lzm <27617839@qq.com>
2021-07-19 14:25:45 +0800

    add tv1

--- app/src/main/java/lzm/com/testforall/Login.java
+++ app/src/main/java/lzm/com/testforall/Login.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Button button,button2;
+        TextView tv1;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         button = (Button) findViewById(R.id.button);
